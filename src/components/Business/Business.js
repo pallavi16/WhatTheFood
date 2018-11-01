2d2e27209c985377ba197ca289706f2af5def672
Pallavi Aggarwal <u1009982@hci.utah.edu>
2018-11-01 13:33:51 -0600

    Changes

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,6 +1,7 @@
  import React from 'react';
  import'./Business.css';
 
+
 const business ={
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
